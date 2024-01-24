b771d52b13e13d4fc4c6e472768b21c3c6cc9593
Sahil singh <sahilsinghiscool@gmail.com>
2024-01-25 01:34:30 +0530

    fixed bugs

--- api/index.js
+++ api/index.js
@@ -14,20 +14,23 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("DB Conncted"))
+  .then(() => console.log("DB Connected"))
   .catch((err) => console.log(err));
 
 app.use(express.json());
 
+// CORS setup
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
     "https://netflix-clone053.netlify.app"
   );
-  res.header(
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
+  res.setHeader("Access-Control-Allow-Credentials", "true");
   next();
 });
 
